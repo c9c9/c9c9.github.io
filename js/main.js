0f3fce3d310529c1eb5d03eb2d8b9b619dc109f1
oiguoga <oiguoga>
2020-07-12 03:01:40 +0800

    项目的开始2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 require.config({
-  timeout:20e3
+  timeout:20e3,
+  paths:{
+    dom:"../libs/dom.min.js?#1"
+  }
 });
-require(['require','../libs/dom.min','./index'],function () {
+require(['require','../libs/dom.min'],function () {
   var $data=$('#ACMD-DST');
   var launch=$data.attr('data-launch')||$data.attr('data-page');
   if(launch){
-    require(launch)
+    require(launch,{baseUrl:''})
   }
 })
